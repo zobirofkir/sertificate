f4a44ff8d2321b8a7a91654200f7900367b6a51d
csw <zobirofkir19@gmail.com>
2025-05-04 17:02:03 +0100

    first commit

---
+++ resources/js/pages/welcome.tsx
@@ -0,0 +1,38 @@
+import { type SharedData } from '@/types';
+import { Head, Link, usePage } from '@inertiajs/react';
+
+export default function Welcome() {
+    const { auth } = usePage<SharedData>().props;
+
+    return (
+        <>
+            <header className='container md:p-4 p-0'>
+                <nav className="flex items-center justify-end gap-4">
+                    {auth.user ? (
+                        <Link
+                            href={route('dashboard')}
+                            className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
+                        >
+                            Dashboard
+                        </Link>
+                    ) : (
+                        <>
+                            <Link
+                                href={route('login')}
+                                className="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]"
+                            >
+                                Log in
+                            </Link>
+                            <Link
+                                href={route('register')}
+                                className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
+                            >
+                                Register
+                            </Link>
+                        </>
+                    )}
+                </nav>
+            </header>
+        </>
+    );
+}
