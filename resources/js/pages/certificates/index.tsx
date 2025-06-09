8a9c32e95b95e2ff7d366f5c3f76078c98182226
csw <zobirofkir19@gmail.com>
2025-06-09 23:52:21 +0100

    change names to download html certificate

--- resources/js/pages/certificates/index.tsx
+++ resources/js/pages/certificates/index.tsx
@@ -168,7 +168,7 @@ const Index = () => {
     }
   };
     
-  const handleDownloadCertificate = async () => {
+  const handleDownloadHtmlCertificate = async () => {
     setIsGenerating(true);
     toast.info("Generating certificate, please wait...");
 
@@ -284,7 +284,7 @@ const Index = () => {
 
         <div className="flex justify-end mb-6">
         <Button
-            onClick={handleDownloadCertificate}
+            onClick={handleDownloadHtmlCertificate}
             className="bg-gradient-to-r from-gray-600 to-gray-800 hover:from-gray-700 hover:to-gray-900 text-white"
             disabled={isGenerating}
           >
@@ -301,7 +301,7 @@ const Index = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
                   <path fillRule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clipRule="evenodd" />
                 </svg>
-                Download Certificate
+                Download Html Certificate
               </>
             )}
           </Button>
